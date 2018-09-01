133c6dc6b1242309a985c5a27153137790e6eef3
austine Iyke <austyns.iyke@gmail.com>
2018-09-01 14:47:59 +0100

    ...

--- 2/second.py
+++ 2/second.py
@@ -123,3 +123,35 @@ def getSumOfAList(number):
 result = getSumOfAList([6, 5, 3, 8, 4, 2, 5, 4, 11])
 
 print(result)
+
+
+# parent class
+class Bird:
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim faster")
+
+# child class
+class Penguin(Bird):
+
+    def __init__(self):
+        # call super() function
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisThis(self):
+        print("Penguin")
+
+    def run(self):
+        print("Run faster")
+
+
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
